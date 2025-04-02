334314c35840b264d8bac8d21b05163964391495
sushmitha321 <bysushmitha1994@gmail.com>
2025-04-02 18:23:38 +0530

    new project commited

---
+++ src/test/java/selenium.java
@@ -0,0 +1,55 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class selenium {
+    public static void main(String[] args) throws InterruptedException, IOException {
+      System.out.println( "my world");
+      //int a=10;
+      //int b=22;
+      //boolean c =a>=b;
+      //System.out.println(c);
+
+//WebDriverManager.chromedriver().setup();
+//    WebDriver driver=new ChromeDriver();
+//    driver.manage().window().maximize();
+//    driver.get("https://www.google.com");
+
+        Properties prop = new Properties();
+        FileInputStream fis = new FileInputStream("./src/test/resources/staging.properties");
+        prop.load(fis);
+
+        String vaa = prop.getProperty("username");
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver driver=new FirefoxDriver();
+        driver.get("https://uat-bp.buenofinanceinc.com/login");
+        String Ex=driver.getCurrentUrl();
+        System.out.println(Ex+"get current url");
+        String TIT=driver.getTitle();
+        System.out.println(TIT+"page title");
+
+        WebElement text=driver.findElement(By.xpath("//span[@class='MuiButton-label']"));
+        String submittext = text.getText();
+        WebElement username=driver.findElement(By.xpath("//input[@id='username']"));
+username.sendKeys(prop.getProperty("username"));
+Thread.sleep(2000);
+username.clear();
+username.sendKeys("raghu");
+
+      TakesScreenshot screenshot = ((TakesScreenshot) driver);
+      File scfile = screenshot.getScreenshotAs(OutputType.FILE);
+
+      FileUtils.copyFile(scfile, new File("./src/test/resources/screenshot.png"));
+
+
+
+
+    }}
